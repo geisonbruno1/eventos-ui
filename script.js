dd4cbb30c40685d7953c84f6b6ebf23fdafdd2e5
geisonbruno1 <geisonbruno0@gmail.com>
2024-02-26 20:01:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputName = document.forms.cadastro.name;
+
+inputName.addEventListener('keyup', handleinputNameKeyUp);
+
+function handleinputNameKeyUp(event) {
+    console.log(event.target.value);
+}
+
+const inputEmail = document.forms.cadastro.email;
+inputEmail.addEventListener('change', handleinputEmailChange);
+
+const btnSubmit = document.querySelector('[type="submit"]');
+btnSubmit.addEventListener('click', handleBtnSubmitClick);
+
+function handleBtnSubmitClick(event) {
+    event.preventDefault();
+console.log(`"Dados: ${inputName.value}, ${inputEmail.value}`);
+}
+
+function handleinputEmailChange(event) {
+    if(validateEmail(event.target.value)) {
+        event.target.classlist.remove("input-error");
+    }
+    else {
+        event.target.classlist.add("input-error");
+    }
+}
+
+// https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+function validateEmail(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  }
